480681b0d48fb57bd71a2abd33ccd0b9fadc9964
Stefan Su <stefansu28@gmail.com>
2017-01-28 16:33:11 -0500

    GRAPHSgit add graph.js index.html!

--- graph.js
+++ graph.js
@@ -1,7 +1,10 @@
 // Artist graph class
 function ArtistNode(name, edges) {
-    this.name = name;
-    this.edges = edges;
+    this.name = name
+    this.edges = edges
+    this.circle = null
+    this.x = null
+    this.y = null
     // this.html = d3.
 }
 
@@ -22,24 +25,57 @@ function ArtistNode(name, edges) {
 var graph
 var circles
 window.onload = function() {
-    var ArtistList = [new ArtistNode('Kayne', [1,2,3]), new ArtistNode('Kendrick', [1,2,3])]
+    var ArtistList = [];
+    ArtistList['Kanye'] = new ArtistNode('Kanye', ['Kendrick'])
+    ArtistList['Kendrick'] = new ArtistNode('Kendrick', ['Kanye'])
+    ArtistList['DJ Khaled'] = new ArtistNode('DJ Khaled', ['Kanye'])
     var svg = d3.select('svg')
-    ArtistList.forEach(function(ele, n) {
-	var group = svg.append('g');
+    for(var name in ArtistList) {
+	ArtistList[name].x = Math.random()*400
+	ArtistList[name].y = Math.random()*400
+    }
+    // Draw the lines
+    var lineFunc = d3.line()
+    var lines = []
+    for(var name in ArtistList) {
+	var firstArtist = ArtistList[name]
+	if(!lines[name])
+	    lines[name] = []
+	for(var edge in firstArtist.edges) {
+	    var other = firstArtist.edges[edge]
+	    if(lines[other] && lines[other][name])
+		continue;
+	    lines[name][other] = true
+	    var otherArtist = ArtistList[other]
+	    var points = [[otherArtist.x,otherArtist.y],
+			  [firstArtist.x,firstArtist.y]]
+	    svg.append('path')
+		.attr('d', lineFunc(points))
+		.attr('stroke', 'black')
+		.attr('stroke-width', 2)
+		.attr('fill', 'none')
+	}
+    }
+
+    // Draw circles
+    for(var name in ArtistList) {
+	var ele = ArtistList[name]
+	var dx = ArtistList[name].x,dy = ArtistList[name].y
+	var group = svg.append('g').attr('id',ele.name);
 	var text = group.append('text')
-	    .attr('dy', 60)
-	    .attr('dx', 55*n)
-	    .html(ele.name);
-	group.append('circle')
+	    .attr('dy', dy)
+	    .attr('dx', dx)
+	    .html(ele.name)
+	ele.circle = group.append('circle')
 	    .attr('r',text.node().getBBox().width/2)
 	    .style('fill', 'steelblue')
-	    .attr('cy', 60)
-	    .attr('cx', 55*n+text.node().getBBox().width/2)
+	    .attr('cy', dy)
+	    .attr('cx', dx)
 	group.append('text')
-	    .attr('dy', 60)
-	    .attr('dx', 55*n)
-	    .html(ele.name);
-	text.remove();
-    })
-    circle = d3.selectAll('circle');
+	    .attr('dy', dy)
+	    .attr('dx', dx-text.node().getBBox().width/2)
+	    .html(ele.name)
+	text.remove()
+    }
+    circle = d3.selectAll('circle')
 }
