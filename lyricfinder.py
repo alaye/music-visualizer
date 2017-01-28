15efee95201b91145ea392168d767cfc4422f9ef
Dangoat <danevans@terpmail.umd.edu>
2017-01-28 12:48:42 -0500

    counts words in lyrics

---
+++ lyricfinder.py
@@ -0,0 +1,18 @@
+input2 = input1.replace(",","")
+input2 = input2.replace(".","")
+input2 = input2.replace("?","")
+input2 = input2.replace(";","")
+input2 = input2.replace("!","")
+input2 = input2.replace("@","")
+lst = input2.split()
+lyrics = {}
+for st in lst:
+    if st == ly:
+        if lyrics.has_key(st)
+            lyrics[st] = lyrics[st] + 1
+        else:
+            lyrics[st] = 1
+
+
+
+
