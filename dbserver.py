68cc147fa91be54c867ab9aa4215cbc4bdfdca07
alaye <alexanderlaye@hotmail.com>
2017-01-29 02:12:22 -0500

    I <3 monogodong

--- dbserver.py
+++ dbserver.py
@@ -3,9 +3,11 @@ from flask import json, jsonify
 import re
 import os
 import urllib
+from pymongo import MongoClient
 app = Flask(__name__)
 
-jdata = json.load(open("merged2/Artists1.json"))
+client = MongoClient('mongodb://admin:admin123@localhost:27017/')
+db = client.test
 
 @app.route('/')
 def hello_world():
@@ -15,7 +17,18 @@ def hello_world():
 def get_file(author):
     real = urllib.unquote(author)
     print real
-    return jsonify(jdata[author])
+    res = db.songs.find({"Artist": author})
+    fin = []
+    for my_dict in res:
+        tmp_dict = {}
+        del my_dict[u'_id']
+        for key in my_dict:
+            newkey = key.encode('ascii', errors="backslashreplace")
+            newval = my_dict[key].encode('ascii', errors=
+                    "backslashreplace")
+            tmp_dict[newkey] = newval
+        fin.append(tmp_dict)
+    return jsonify({"Songs": fin})
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
