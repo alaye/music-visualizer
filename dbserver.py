b32d93e0a6eabd98db5ca98bf10c92feba9ffd63
alaye <alexanderlaye@hotmail.com>
2017-01-29 02:30:23 -0500

    added dans bs old code I'm mad

--- dbserver.py
+++ dbserver.py
@@ -30,5 +30,56 @@ def get_file(author):
         fin.append(tmp_dict)
     return jsonify({"Songs": fin})
 
+@app.route('/relations/', methods=['POST'])
+def relations():
+    artist=request.form['artist']
+    nodes = []
+    edges = []
+
+    anchor = artist
+
+    q = [anchor]
+    art = None
+
+    while len(q) > 0:
+        art = q.pop(0)
+        #get song list
+        for song in songlist:
+            #get string of featured artists
+            m = re.match('feat. ([^)]+)')
+            if m:
+                s = m.group()
+                #split into artists
+                lst = s.split(",")
+                if len(lst) > 1:
+                    lstend = (lst.pop()).split("&")
+                    lst = lst.extend(lstend)
+                for a in lst:
+                    a = a.strip()
+                    edges.append((art,a))
+                    if nodes.count(a) == 0:
+                        q.append(a)
+                    for b in lst:
+                        if a != b:
+                            edges.append((a,b))
+
+        nodes.append(art)
+
+    i = 0
+    j = 0
+    while i < len(edges):
+        j = i+1
+        t1 = edges[i]
+        while j < len(edges):
+            t2 = edges[j]
+            if t1[1] == t2[1] and t1[2] == t2[2]:
+                edges.remove(t2)
+            elif t1[2] == t2[1] and t1[1] == t2[1]:
+                edges
+            else:
+                j = j + 1
+        i = i + 1
+    
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0')
